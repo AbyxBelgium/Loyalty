268ef467f033a7013b7bb0e7b3ef3ee73f9f7d79
pverscha <Pieter.Verschaffelt@UGent.be>
2017-02-05 15:37:30 +0100

    Started working on edit-card-function. To complete this feature, we must change our storage solution to a database-based system.

--- app/src/main/java/com/abyx/loyalty/activities/CardActivity.java
+++ app/src/main/java/com/abyx/loyalty/activities/CardActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.abyx.loyalty.contents.Card;
 import com.abyx.loyalty.extra.Constants;
@@ -26,5 +28,24 @@ public class CardActivity extends AppCompatActivity {
             CardFragment fragment = CardFragment.newInstance((Card) data.getParcelableExtra(Constants.INTENT_CARD_ARG));
             getSupportFragmentManager().beginTransaction().add(R.id.cardContainer, fragment).commit();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_card, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_edit) {
+            Intent intent = new Intent(CardActivity.this, EditActivity.class);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
