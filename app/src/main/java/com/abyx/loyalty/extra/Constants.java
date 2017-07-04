6465ddb3d00f07172b1947f8c9684c97e779cee1
pverscha <Pieter.Verschaffelt@UGent.be>
2017-07-04 11:55:27 +0200

    Implemented DetailedLogoTask

--- app/src/main/java/com/abyx/loyalty/extra/Constants.java
+++ app/src/main/java/com/abyx/loyalty/extra/Constants.java
@@ -35,6 +35,7 @@ public class Constants {
 
     // The background colour for a store's logo shown in a circle.
     public static final int LOGO_BACKGROUND_COLOUR = Color.argb(143, 175, 175, 175);
+    public static final float MAGIC_CROP_TOLERANCE = 0.2f;
 
     // Name of the directory that contains all raw, unprocessed logo's (thus without any background
     // or magic crop).
--- app/src/main/java/com/abyx/loyalty/tasks/DetailedLogoTask.java
+++ app/src/main/java/com/abyx/loyalty/tasks/DetailedLogoTask.java
@@ -18,31 +18,107 @@ package com.abyx.loyalty.tasks;
 
 import android.content.Context;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.AsyncTask;
 import android.support.annotation.Nullable;
 
 import com.abyx.loyalty.contents.Card;
+import com.abyx.loyalty.extra.Constants;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import be.abyx.aurora.CircleShape;
+import be.abyx.aurora.ImageUtils;
+import be.abyx.aurora.ParallelShapeFactory;
+import be.abyx.aurora.ShapeFactory;
 
 /**
  * Retrieves the logo corresponding to this store that has been processed with a magic crop and that
- * is placed above a circular, semi-transparent, background. This task will automatically check
- * if a logo has already been found for this store and will download it once found. The Aurora
- * library is used for editing these images on the fly.
+ * is placed above a circular, semi-transparent, background.
+ *
+ * The following exceptions might occur and should be caught by the implementation of the
+ * listener's onFailed()-method:
+ * <p>
+ *     <ul>
+ *         <li>IOException</li>
+ *     </ul>
+ * </p>
  *
  * @author Pieter Verschaffelt
  */
-public class DetailedLogoTask extends AsyncTask<Card, Void, Bitmap> {
+public class DetailedLogoTask extends AsyncTask<Bitmap, Void, Bitmap> {
     private Context context;
     private TaskListener<Bitmap> listener;
+    private Card card;
 
-    public DetailedLogoTask(Context context, TaskListener<Bitmap> listener) {
+    public DetailedLogoTask(Context context, TaskListener<Bitmap> listener, Card card) {
         this.context = context;
         this.listener = listener;
+        this.card = card;
+    }
+
+    /**
+     * This method will produce the desired logo on circular background.
+     *
+     * @param params An array of length one that contains the raw logo Bitmap. This Bitmap will then
+     *               be used as the foreground layer for the output Bitmap.
+     * @return A circular logo placed upon a semi-transparent background.
+     */
+    @Override
+    @Nullable
+    protected Bitmap doInBackground(Bitmap... params) {
+        String fileName = Constants.CACHE_DIR_LOGO_DETAIL + Integer.toString(card.getName().hashCode()) + "." + Constants.IMAGE_FORMAT;
+
+        File file = context.getFileStreamPath(fileName);
+
+        Bitmap output;
+
+        if (file == null || !file.exists()) {
+            // Create the desired output from scratch and save it in persistent storage.
+            Bitmap logo = params[0];
+            ImageUtils utils = new ImageUtils(this.context);
+            Bitmap croppedLogo = utils.magicCrop(logo, Color.WHITE, Constants.MAGIC_CROP_TOLERANCE);
+            ShapeFactory shapeFactory = new ParallelShapeFactory();
+            output = shapeFactory.createShape(new CircleShape(this.context), croppedLogo, Constants.LOGO_BACKGROUND_COLOUR, 150);
+
+            // Save file in persistent storage
+            try (FileOutputStream fos = context.openFileOutput(fileName, Context.MODE_PRIVATE)) {
+                output.compress(Constants.IMAGE_COMPRESS_FORMAT, Constants.IMAGE_QUALITY, fos);
+            } catch (IOException e) {
+                this.listener.onFailed(e);
+                return null;
+            }
+
+        } else {
+            // Retrieve previously generated file from storage.
+            try (FileInputStream in = context.openFileInput(fileName)) {
+                output = BitmapFactory.decodeStream(in);
+            } catch (IOException e) {
+                this.listener.onFailed(e);
+                return null;
+            }
+        }
+
+        return output;
     }
 
     @Override
-    @Nullable
-    protected Bitmap doInBackground(Card... params) {
-        return null;
+    protected void onPreExecute() {
+        super.onPreExecute();
+        this.listener.onProgressUpdate(0.0);
+    }
+
+    @Override
+    protected void onPostExecute(Bitmap bitmap) {
+        super.onPostExecute(bitmap);
+        if (bitmap != null) {
+            this.listener.onProgressUpdate(1.0);
+            this.listener.onDone(bitmap);
+        }
     }
 }
