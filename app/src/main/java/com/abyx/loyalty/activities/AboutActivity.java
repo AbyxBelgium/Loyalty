4ebe107e367e21cfacd7282d3051be4d58f0040a
pverscha <Pieter.Verschaffelt@UGent.be>
2017-07-25 19:19:46 +0200

    Redesign of AboutActivity and BackupActivity

--- app/src/main/java/com/abyx/loyalty/activities/AboutActivity.java
+++ app/src/main/java/com/abyx/loyalty/activities/AboutActivity.java
@@ -20,12 +20,18 @@ import android.content.pm.PackageManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.abyx.loyalty.R;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * This activity shows the about screen where the user can find all sorts of developer related
@@ -47,6 +53,25 @@ public class AboutActivity extends AppCompatActivity {
         } catch (PackageManager.NameNotFoundException e){
             //The version number is simply not shown when something goes wrong. No further
             //information has to be given here.
+        }
+        TextView licenseTextView = (TextView) findViewById(R.id.licenseTextView);
+        licenseTextView.setText(getLicenseText());
+    }
+
+    private String getLicenseText() {
+        InputStream inputStream = getResources().openRawResource(R.raw.license);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            StringBuilder builder = new StringBuilder();
+            String line = reader.readLine();
+            while (line != null) {
+                builder.append(line);
+                builder.append("\n");
+                line = reader.readLine();
+            }
+            return builder.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
         }
     }
 }
